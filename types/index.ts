f8522f037ab6761c20564c491cfe6d38168d43b8
Brian Holt <bholtbholt@icloud.com>
2020-08-15 01:32:04 +0200

    Remove unused types

--- types/index.ts
+++ types/index.ts
@@ -8,18 +8,10 @@ export interface HistoryEvent {
   id: UUID;
 }
 
-export interface Listeners {
-  [key: string]: Function;
-}
-
 export interface Player {
   avatar: string;
   name: string;
   id: PlayerId;
 }
 
-declare global {
-  interface Window {
-    historyEvents: HistoryEvent[];
-  }
 }
