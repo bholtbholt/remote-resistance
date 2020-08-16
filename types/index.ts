359ccc26ac236d6c7f692517a33c03ff587b34fd
Brian Holt <bholtbholt@icloud.com>
2020-08-16 09:33:04 +0200

    Add remaining game types

--- types/index.ts
+++ types/index.ts
@@ -3,6 +3,13 @@ export type Gamestate = 'PRE_GAME' | 'IN_GAME' | 'POST_GAME';
 export type PlayerId = UUID;
 type UUID = string;
 
+export interface Game {
+  rounds: Round[];
+  players: PlayerId[];
+  spies: PlayerId[];
+  winner: 'resistance' | 'spies';
+}
+
 export interface HistoryEvent {
   action: Action;
   data: any;
@@ -10,10 +17,29 @@ export interface HistoryEvent {
   id: UUID;
 }
 
+export interface MissionPhase {
+  team: PlayerId[];
+  votes: MissionVote[];
+  result: 'successful' | 'failed';
+}
+
+export interface MissionVote {
+  playerId: PlayerId;
+  vote: 'success' | 'fail';
+}
+
 export interface Player {
   avatar: string;
   name: string;
   id: PlayerId;
+}
+
+export interface Round {
+  teamBuildingPhase: TeamBuildingPhase[];
+  missionPhase: MissionPhase;
+  voteTrackMaximum: number;
+  voteTrack: number;
+  winner: 'resistance' | 'spies';
 }
 
 export interface Ruleset {
@@ -26,4 +52,14 @@ export interface Ruleset {
   roundsToWin: number;
 }
 
+export interface TeamBuildingPhase {
+  leader: PlayerId;
+  team: PlayerId[];
+  votes: TeamVote[];
+  result: 'approved' | 'rejected';
+}
+
+export interface TeamVote {
+  playerId: PlayerId;
+  vote: 'approve' | 'reject';
 }
