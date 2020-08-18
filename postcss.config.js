5294d59ec52b02b9a4c07c6c893ee2716282028d
Brian Holt <bholtbholt@icloud.com>
2020-08-18 18:04:32 +0200

    Add custom style

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,3 @@
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+  plugins: ['tailwindcss', 'postcss-nested', 'autoprefixer'],
 };
