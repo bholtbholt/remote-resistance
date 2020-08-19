e4e04d4c523b5622be06e2575e40203b2d802079
Brian Holt <bholtbholt@icloud.com>
2020-08-19 15:48:58 +0200

    Add PlayerForm tests

---
+++ client/js/__tests__/PlayerForm.test.ts
@@ -0,0 +1,66 @@
+import 'ts-jest';
+import { render, fireEvent } from '@testing-library/svelte';
+import AppFixture from './AppFixture.svelte';
+import PlayerForm from '../components/PlayerForm.svelte';
+import { currentPlayerId, players } from '../components/player-store';
+const socket = require('socket.io-client')('test');
+
+test('should enable the form by default', () => {
+  const { getByText, getByLabelText } = render(AppFixture, { socket, component: PlayerForm });
+
+  const nameField = getByLabelText('Name:') as HTMLInputElement;
+  const button = getByText('Join game') as HTMLButtonElement;
+
+  expect(nameField.disabled).toEqual(false);
+  expect(button.disabled).toEqual(false);
+});
+
+test('should disable the button on submit', async () => {
+  const { getByText } = render(AppFixture, { socket, component: PlayerForm });
+
+  const button = getByText('Join game') as HTMLButtonElement;
+  await fireEvent.submit(button.form);
+
+  expect(button.disabled).toEqual(true);
+});
+
+test('should set the current player', async () => {
+  spyOn(currentPlayerId, 'set');
+  const { getByText, getByLabelText } = render(AppFixture, { socket, component: PlayerForm });
+
+  const nameField = getByLabelText('Name:') as HTMLInputElement;
+  const button = getByText('Join game') as HTMLButtonElement;
+
+  await fireEvent.submit(button.form);
+
+  expect(currentPlayerId.set).toBeCalled();
+});
+
+test('should add the player to the room', async () => {
+  spyOn(socket, 'emit');
+  const { getByText, getByLabelText } = render(AppFixture, { socket, component: PlayerForm });
+
+  const nameField = getByLabelText('Name:') as HTMLInputElement;
+  nameField.value = 'Binks';
+  const button = getByText('Join game') as HTMLButtonElement;
+
+  await fireEvent.submit(button.form);
+
+  expect(socket.emit).toHaveBeenCalledWith(
+    'player::add',
+    expect.objectContaining({ name: 'Binks', avatar: '🐶' }),
+  );
+});
+
+test('should disable avatars that players have selected', () => {
+  players['player::add']({ id: '1234', avatar: '🐶', name: 'Binks' });
+  const { getByText, getByRole } = render(AppFixture, { socket, component: PlayerForm });
+
+  const button = getByText('Join game') as HTMLButtonElement;
+  const dogAvatar = getByRole('radio', { name: '🐶' }) as HTMLInputElement;
+  const catAvatar = getByRole('radio', { name: '🐱' }) as HTMLInputElement;
+
+  expect(dogAvatar.disabled).toEqual(true);
+  expect(catAvatar.disabled).toEqual(false);
+  expect(catAvatar.checked).toEqual(true);
+});
