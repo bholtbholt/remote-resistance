6aa30ff6dcfb4916208d25acde06adbc4668213b
Brian Holt <bholtbholt@icloud.com>
2020-08-22 15:00:34 +0200

    Add transitions between lobbies

--- client/js/components/LobbyGame.svelte
+++ client/js/components/LobbyGame.svelte
@@ -1,1 +1,19 @@
-<p>Game lobby</p>
+<script type="text/typescript" lang="ts">
+  import { fly, fade } from 'svelte/transition';
+
+  import RoundTracker from './RoundTracker.svelte';
+  import RevealRole from './RevealRole.svelte';
+
+  let showRoles = true;
+</script>
+
+<div id="LobbyGame" in:fade>
+  <RoundTracker />
+  {#if showRoles}
+    <RevealRole />
+    <div class="mx-lg" in:fly="{{ y: 200, duration: 600, delay: 3000 }}">
+      <button on:click|preventDefault="{() => { showRoles = false }}"
+        class="btn-primary font-bold text-lg w-full">Start first round</button>
+    </div>
+  {/if}
+</div>
