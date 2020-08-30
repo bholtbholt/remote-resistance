1449294a095a72869d6898500cfb6e5d9347bbf9
Brian Holt <bholtbholt@icloud.com>
2020-08-30 23:18:17 +0200

    Create rotate transition

--- components/custom-transitions.ts
+++ components/custom-transitions.ts
@@ -1,4 +1,4 @@
-function blur(node, { delay = 0, duration = 1000, amount = 0.6 }) {
+export function blur(node, { delay = 0, duration = 1000, amount = 0.6 }) {
   const f = getComputedStyle(node).filter.replace(/^none$/, '');
 
   return {
@@ -8,4 +8,20 @@ function blur(node, { delay = 0, duration = 1000, amount = 0.6 }) {
   };
 }
 
-export { blur };
+export function rotate(
+  node,
+  { delay = 0, duration = 400, x = -160, y = 100, deg = -30, origin = 'top right' },
+) {
+  const style = getComputedStyle(node);
+  const transform = style.transform === 'none' ? '' : style.transform;
+
+  return {
+    delay,
+    duration,
+    css: (t, u) => `
+      transform: ${transform}
+        rotate(${(1 - t) * deg}deg)
+        translate(${(1 - t) * x}%, ${(1 - t) * y}%);
+      transform-origin: ${origin};`,
+  };
+}
