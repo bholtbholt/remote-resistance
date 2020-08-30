2ff5a46f85710467ea8d4561bea5fe7633433664
Brian Holt <bholtbholt@icloud.com>
2020-08-30 23:17:56 +0200

    Save reveal state in sessionStorage so it doesn't keep popping up

--- components/LobbyGame.svelte
+++ components/LobbyGame.svelte
@@ -5,18 +5,22 @@
   import RevealRole from './RevealRole.svelte';
   import TeamBuilding from './TeamBuilding.svelte';
 
-  let showRoles = true;
+  let hideRoleReveal = window.sessionStorage.getItem('hideRoleReveal');
+  function hideRoles() {
+    hideRoleReveal = true;
+    window.sessionStorage.setItem('hideRoleReveal', true);
+  }
 </script>
 
 <div id="LobbyGame" in:fade>
   <RoundTracker />
-  {#if showRoles}
+  {#if hideRoleReveal}
+    <TeamBuilding />
+  {:else}
     <RevealRole />
     <div class="mx-lg" in:fly="{{ y: 200, duration: 600, delay: 3000 }}">
-      <button on:click|preventDefault="{() => { showRoles = false }}"
+      <button on:click|preventDefault="{hideRoles}"
         class="btn-primary font-bold text-lg w-full">Start first round</button>
     </div>
-  {:else}
-    <TeamBuilding />
   {/if}
 </div>
