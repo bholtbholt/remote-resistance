46f7b9811a46ea5dbcbd5adc0972aac6eba53f78
Brian Holt <bholtbholt@icloud.com>
2020-08-31 19:27:26 +0200

    Change cardFlip transition

--- components/RoleCard.svelte
+++ components/RoleCard.svelte
@@ -1,7 +1,7 @@
 <script type="text/typescript" lang="ts">
   export let showCard = false;
   import { fly } from 'svelte/transition';
-  import { rotate } from './custom-transitions';
+  import { cardFlip } from './custom-transitions';
   import { currentPlayer, playerIsASpy } from '../stores/player';
 
   import Player from './Player.svelte';
@@ -13,8 +13,8 @@
 </script>
 
 {#if showCard}
-  <div class="fixed inset-0 cursor-pointer background-fail-700" on:click>
-    <div class="fixed top-0 inset-x-0 z-10 m-xl rounded-lg shadow-xl border-solid border-xl {borderColor}" transition:rotate>
+  <div class="fixed inset-0 z-10 cursor-pointer background-fail-700" on:click>
+    <div class="fixed top-0 inset-x-0 m-xl rounded-lg shadow-xl border-solid border-xl {borderColor}" transition:cardFlip>
       <div class="text-center bg-white rounded-lg p-lg pt-xl {outlineColor}">
         <h2 class="text-xl font-extrabold -mb-lg {headingColor}">{headingText}</h2>
         <svg viewBox="0 0 20 20">
--- components/custom-transitions.ts
+++ components/custom-transitions.ts
@@ -1,3 +1,5 @@
+import { cubicOut } from 'svelte/easing';
+
 export function blur(node, { delay = 0, duration = 1000, amount = 0.6 }) {
   const f = getComputedStyle(node).filter.replace(/^none$/, '');
 
@@ -5,6 +7,23 @@ export function blur(node, { delay = 0, duration = 1000, amount = 0.6 }) {
     delay,
     duration,
     css: (t, u) => `opacity: ${t}; filter: ${f} blur(${u * amount}em)`,
+  };
+}
+
+export function cardFlip(node, { delay = 0, duration = 400, flip = false }) {
+  const style = getComputedStyle(node);
+  const transform = style.transform === 'none' ? '' : style.transform;
+  const [x, deg, origin] = flip ? [160, 30, 'left'] : [-160, -30, 'right'];
+
+  return {
+    delay,
+    duration,
+    easing: cubicOut,
+    css: (t, u) => `
+      transform: ${transform}
+        rotate(${(1 - t) * deg}deg)
+        translate(${(1 - t) * x}%, ${(1 - t) * 100}%);
+      transform-origin: top ${origin};`,
   };
 }
 
