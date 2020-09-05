5bb154ac078e9ab5f6b945b4d0b789c00c9e8963
Brian Holt <bholtbholt@icloud.com>
2020-09-05 12:24:30 +0200

    Add RoundTracker system tests

---
+++ tests/RoundTracker.test.ts
@@ -0,0 +1,50 @@
+import 'ts-jest';
+import { render } from '@testing-library/svelte';
+import { resetTestState } from './test-helper';
+import AppFixture from './AppFixture.svelte';
+import RoundTracker from '../components/RoundTracker.svelte';
+import { roundOneStart, roundOneNewVote } from './history-states';
+const socket = require('socket.io-client')('test');
+
+afterEach(() => {
+  return resetTestState();
+});
+
+test('should show all rounds', () => {
+  const { container } = render(AppFixture, {
+    socket,
+    component: RoundTracker,
+    historyState: roundOneStart,
+  });
+
+  const tracker = container.querySelectorAll('#RoundTracker > li');
+
+  expect(tracker.length).toEqual(5);
+});
+
+test('should highlight the current round', () => {
+  const { container } = render(AppFixture, {
+    socket,
+    component: RoundTracker,
+    historyState: roundOneStart,
+  });
+
+  const tracker = container.querySelectorAll('#RoundTracker > li');
+  const [round1, round2] = tracker;
+
+  expect(round1.classList).toContain('bg-warning-400');
+  expect(round2.classList).not.toContain('bg-warning-400');
+});
+
+test('should show failed team votes for the currrent round', () => {
+  const { container } = render(AppFixture, {
+    socket,
+    component: RoundTracker,
+    historyState: roundOneNewVote,
+  });
+
+  const round1 = container.querySelector('#RoundTracker > li:first-of-type');
+
+  expect(round1.classList).toContain('bg-warning-400');
+  expect(round1.innerHTML).toContain('<div class="font-bold text-sm text-warning-800">1</div>');
+});
