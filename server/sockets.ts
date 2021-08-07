370db91259c9fff0b99f3a19711e571760f96ed8
Brian Holt <bholtbholt@icloud.com>
2021-08-07 12:41:23 -0700

    Upgrade dependencies

--- server/sockets.ts
+++ server/sockets.ts
@@ -1,5 +1,5 @@
 import type { Action } from '../types';
-import * as socketIO from 'socket.io';
+import { Server as SocketIO } from 'socket.io';
 import { server } from './routes';
 import { actionNames } from '../actions';
 import { historyEvents, createHistory } from './history';
@@ -8,7 +8,7 @@ const initHistory = historyState[process.env.HISTORY] || [];
 
 // Rooms are actually "namespaces" in socket.io for better security
 // not to be confused with socket.io "rooms"
-const rooms = socketIO(server).of(/^\/\w+/);
+const rooms = new SocketIO(server).of(/^\/\w+/);
 rooms.on('connection', (socket) => {
   const { nsp: room } = socket;
   // Share socket events through history. The app listens for
