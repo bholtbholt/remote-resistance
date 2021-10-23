ddd116bfb807c7d9c3ed77df6fdd0f3417ba98c3
bholtbholt <bholtbholt@icloud.com>
2021-10-23 15:57:45 -0700

    Add copy url button

---
+++ components/ButtonCopy.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let copyText = window.location.href;
+  let success = 'Coped!';
+  let copying = false;
+
+  async function share() {
+    await navigator.clipboard.writeText(copyText);
+
+    copying = true;
+    setTimeout(() => {
+      copying = false;
+    }, 1000);
+  }
+</script>
+
+<button type="button" on:click={share}>
+  {#if copying}
+    {success}
+  {:else}
+    <slot>Copy text</slot>
+  {/if}
+</button>
