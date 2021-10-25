7eb75de5d43d20caa4ea380f18a2d323b299dd9c
bholtbholt <bholtbholt@icloud.com>
2021-10-24 17:33:54 -0700

    Modularize sockets + routes

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,7 @@
 require('dotenv').config();
-import './routes';
-import './sockets';
+import { Server as SocketIO } from 'socket.io';
+import { expressServer } from './routes';
+import { socketConnection } from './sockets';
+
+const namespaces = new SocketIO(expressServer).of(/^\/\w+/);
+socketConnection(namespaces);
