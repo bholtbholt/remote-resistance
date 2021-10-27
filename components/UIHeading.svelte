eb57cd5ca273a126e8b84badc890ae3f24736bb1
bholtbholt <bholtbholt@icloud.com>
2021-10-26 20:20:59 -0700

    Update reveal role

---
+++ components/UIHeading.svelte
@@ -0,0 +1,10 @@
+<header class="mb-8 text-center">
+  <h2 class="text-3xl tracking-tight font-semibold text-indigo-200 dark:text-gray-300">
+    <slot />
+  </h2>
+  {#if $$slots.subheading}
+    <h3 class="text-xl mt-2 text-indigo-300 dark:text-gray-400">
+      <slot name="subheading" />
+    </h3>
+  {/if}
+</header>
