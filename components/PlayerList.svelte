627b1e526607cda60fde3dec92b25aa0e9acbe69
bholtbholt <bholtbholt@icloud.com>
2021-10-27 19:39:45 -0700

    Create PlayerList component

---
+++ components/PlayerList.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let cols = 5;
+  export let players = [];
+  import Player from './Player.svelte';
+  import { gridSize } from './view-helper';
+</script>
+
+<ul id="playerList" class="grid {gridSize(cols)} gap-2">
+  {#each players as player}
+    <Player {...player} />
+  {/each}
+</ul>
