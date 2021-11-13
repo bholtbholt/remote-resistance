f3491f42daf49b6bdb5dc341f548bbe95f827ac5
bholtbholt <bholtbholt@icloud.com>
2021-11-13 15:19:06 -0800

    Add win states from game end

--- tests/PhaseMissionReveal.test.ts
+++ tests/PhaseMissionReveal.test.ts
@@ -2,58 +2,148 @@ import { render, fireEvent } from '@testing-library/svelte';
 import AppFixture from './AppFixture.svelte';
 import PhaseMissionReveal from '../components/PhaseMissionReveal.svelte';
 import { currentPlayerId } from '../stores/player';
-import { roundOneMissionPassed, players } from './history-states';
+import {
+  roundOneMissionFailed,
+  roundOneMissionPassed,
+  roundFiveMissionFailed,
+  roundFiveMissionPassed,
+  players,
+} from './history-states';
 const socket = require('socket.io-client')('test');
 
 import { get } from 'svelte/store';
 import { rounds, currentRound } from '../stores/round';
 
-test('should let the leader start the new round', async () => {
-  jest.spyOn(socket, 'emit');
-  const [previousLeader, newLeader] = players;
-  currentPlayerId.set(newLeader.id);
-  const { queryByText } = render(AppFixture, {
-    socket,
-    historyState: roundOneMissionPassed,
-    component: PhaseMissionReveal,
+describe('when the round ends', () => {
+  test('should show Mission Passed', () => {
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundOneMissionPassed,
+      component: PhaseMissionReveal,
+    });
+
+    expect(queryByText('Mission passed')).toBeInTheDocument();
   });
 
-  const button = queryByText('Start next round');
-  await fireEvent.click(button);
+  test('should show Mission Failed', () => {
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundOneMissionFailed,
+      component: PhaseMissionReveal,
+    });
 
-  expect(socket.emit).toHaveBeenCalledWith('phase::set', 'TEAM_SELECTION');
+    expect(queryByText('Mission failed')).toBeInTheDocument();
+  });
+
+  test('should let the leader start the new round', async () => {
+    jest.spyOn(socket, 'emit');
+    const [previousLeader, newLeader] = players;
+    currentPlayerId.set(newLeader.id);
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundOneMissionPassed,
+      component: PhaseMissionReveal,
+    });
+
+    const button = queryByText('Start next round');
+    await fireEvent.click(button);
+
+    expect(socket.emit).toHaveBeenCalledWith('phase::set', 'TEAM_SELECTION');
+  });
+
+  test('should reset the round state', async () => {
+    jest.spyOn(socket, 'emit');
+    const [previousLeader, newLeader] = players;
+    currentPlayerId.set(newLeader.id);
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundOneMissionPassed,
+      component: PhaseMissionReveal,
+    });
+
+    const button = queryByText('Start next round');
+    await fireEvent.click(button);
+
+    expect(socket.emit).toHaveBeenCalledWith('missionvote::reset');
+    expect(socket.emit).toHaveBeenCalledWith('team::reset');
+    expect(socket.emit).toHaveBeenCalledWith('teamvote::reset');
+  });
+
+  test('should increment the round', async () => {
+    jest.spyOn(socket, 'emit');
+    const [previousLeader, newLeader] = players;
+    currentPlayerId.set(newLeader.id);
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundOneMissionPassed,
+      component: PhaseMissionReveal,
+    });
+
+    const button = queryByText('Start next round');
+    await fireEvent.click(button);
+
+    expect(socket.emit).toHaveBeenCalledWith('rounds::increment');
+  });
 });
 
-test('should reset the round state', async () => {
-  jest.spyOn(socket, 'emit');
-  const [previousLeader, newLeader] = players;
-  currentPlayerId.set(newLeader.id);
-  const { queryByText } = render(AppFixture, {
-    socket,
-    historyState: roundOneMissionPassed,
-    component: PhaseMissionReveal,
+describe('when spies win', () => {
+  test('should show Spies Win', () => {
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundFiveMissionFailed,
+      component: PhaseMissionReveal,
+    });
+
+    expect(queryByText('Spies win')).toBeInTheDocument();
   });
 
-  const button = queryByText('Start next round');
-  await fireEvent.click(button);
+  test('should start a new game', async () => {
+    jest.spyOn(socket, 'emit');
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundFiveMissionFailed,
+      component: PhaseMissionReveal,
+    });
 
-  expect(socket.emit).toHaveBeenCalledWith('missionvote::reset');
-  expect(socket.emit).toHaveBeenCalledWith('team::reset');
-  expect(socket.emit).toHaveBeenCalledWith('teamvote::reset');
+    const button = queryByText('Start a new game');
+    await fireEvent.click(button);
+
+    expect(socket.emit).toHaveBeenCalledWith('appstate::set', 'PRE_GAME');
+    expect(socket.emit).toHaveBeenCalledWith('missionvote::reset');
+    expect(socket.emit).toHaveBeenCalledWith('team::reset');
+    expect(socket.emit).toHaveBeenCalledWith('teamvote::reset');
+    expect(socket.emit).toHaveBeenCalledWith('phase::set', 'TEAM_SELECTION');
+    expect(socket.emit).toHaveBeenCalledWith('rounds::reset');
+  });
 });
 
-test('should increment the round', async () => {
-  jest.spyOn(socket, 'emit');
-  const [previousLeader, newLeader] = players;
-  currentPlayerId.set(newLeader.id);
-  const { queryByText } = render(AppFixture, {
-    socket,
-    historyState: roundOneMissionPassed,
-    component: PhaseMissionReveal,
+describe('when resistance wins', () => {
+  test('should show Resistance Wins', () => {
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundFiveMissionPassed,
+      component: PhaseMissionReveal,
+    });
+
+    expect(queryByText('Resistance wins')).toBeInTheDocument();
   });
 
-  const button = queryByText('Start next round');
-  await fireEvent.click(button);
+  test('should start a new game', async () => {
+    jest.spyOn(socket, 'emit');
+    const { queryByText } = render(AppFixture, {
+      socket,
+      historyState: roundFiveMissionPassed,
+      component: PhaseMissionReveal,
+    });
 
-  expect(socket.emit).toHaveBeenCalledWith('rounds::increment');
+    const button = queryByText('Start a new game');
+    await fireEvent.click(button);
+
+    expect(socket.emit).toHaveBeenCalledWith('appstate::set', 'PRE_GAME');
+    expect(socket.emit).toHaveBeenCalledWith('missionvote::reset');
+    expect(socket.emit).toHaveBeenCalledWith('team::reset');
+    expect(socket.emit).toHaveBeenCalledWith('teamvote::reset');
+    expect(socket.emit).toHaveBeenCalledWith('phase::set', 'TEAM_SELECTION');
+    expect(socket.emit).toHaveBeenCalledWith('rounds::reset');
+  });
 });
