4f6de96bbed4c5aeebce1ddd896497f5f5b61e10
Brian Holt <bholtbholt@icloud.com>
2021-11-15 21:22:05 -0800

    SECURITY: Add Helmet for best practices (#59)
    * Add helmet for Express security
    
    * Update package.json

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import sslRedirect from 'heroku-ssl-redirect';
+import helmet from 'helmet';
 
 const app = express();
 const expressServer = http.createServer(app);
@@ -8,6 +9,31 @@ const port = process.env.PORT || 4000;
 
 // enable ssl redirect
 app.use(sslRedirect());
+// Follow Express security best practices
+// https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+app.use(
+  helmet({
+    // These directives are required because of dev env, specifically ws: + http:
+    // helemt defaults will work when localhost is upgraded to https
+    contentSecurityPolicy: {
+      directives: {
+        'base-uri': ["'self'"],
+        'block-all-mixed-content': [],
+        'connect-src': ['self', 'wss:', 'ws:', 'http:', 'https:'],
+        'default-src': ["'self'"],
+        'font-src': ["'self'", 'https:', 'data:'],
+        'frame-ancestors': ["'self'"],
+        'img-src': ["'self'", 'data:'],
+        'object-src': ["'none'"],
+        'script-src': ["'self'", 'http:'],
+        'script-src-attr': ["'none'"],
+        'style-src': ["'self'", 'https:', "'unsafe-inline'", 'http:'],
+        // 'upgrade-insecure-requests': [],
+      },
+    },
+  }),
+);
+
 // serves the assets directory
 app.use(express.static('dist'));
 // supports :room_id param
